2468d4ff5169f6e210e352788ef6d9c9c0983b09
melissacabral <melissacabral@gmail.com>
2014-05-28 08:28:06 -0700

    first commit
    after day 3

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+//turn on sleeping WP features
+add_theme_support( 'post-thumbnails' );
+
+
+//additional image sizes
+//				  name, width, height, crop?
+add_image_size( 'banner', 1100, 330, true );
+
+//no close php
